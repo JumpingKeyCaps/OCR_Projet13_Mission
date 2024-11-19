8969ec3c8428dc6bb2d27df8c93b221bd3da47be
Samuel Lebailly <sam.meusa@gmail.com>
2024-11-20 00:15:25 +0100

    debug(ui): Setup a simple test set on the fab click to add an user in database + some logs

--- app/src/main/java/com/openclassrooms/hexagonal/games/ui/MainActivity.kt
+++ app/src/main/java/com/openclassrooms/hexagonal/games/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.openclassrooms.hexagonal.games.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
@@ -8,7 +9,10 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
 import com.openclassrooms.hexagonal.games.screen.Screen
 import com.openclassrooms.hexagonal.games.screen.ad.AddScreen
 import com.openclassrooms.hexagonal.games.screen.auth.AuthenticationScreen
@@ -68,11 +72,46 @@ fun HexagonalGamesNavHost(navHostController: NavHostController,startDestination:
           },
           onMyAccountClick = {
             navHostController.navigate(Screen.MyAccount.route)
-          },
-          onFABClick = {
-            navHostController.navigate(Screen.AddPost.route)
           }
-        )
+        ) {
+
+
+
+//todo -- simple test add entry in DB
+
+          val currentUser = FirebaseAuth.getInstance().currentUser
+          if (currentUser == null) {
+            Log.e("DBs", "No user is authenticated!")
+          } else {
+            Log.d("DBs", "Authenticated user: ${currentUser.uid}")
+
+
+            val db = Firebase.firestore
+            val db2 = FirebaseFirestore.getInstance()
+            Log.d("DBs", "TRY UPDATE DB ...")
+            db.collection("user")
+              .document(currentUser.uid)
+              .set(mapOf("firstname" to "tester","lastname" to "family"))
+              .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                  Log.d("DBs", "User successfully added to Firestore.")
+                } else {
+                  Log.e("DBs","Error task adding user to Firestore: ${task.exception}",task.exception)
+                }
+              }
+              .addOnFailureListener { exception ->
+                Log.e("DBs", "Error adding user to Firestore: $exception", exception)
+              }
+
+
+          }
+
+//todo to uncomment after test ---------------------
+
+//            navHostController.navigate(Screen.AddPost.route)
+
+
+        }
       }
       //-- Add Post Screen
       composable(route = Screen.AddPost.route) {
